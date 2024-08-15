56092465988697d6294d5bcf6276d0fb8f1a2a00
Wolf Mermelstein <wolfmermelstein@gmail.com>
2024-08-15 00:45:24 -0400

    Add cookiecutter script and build with nix

---
+++ cookiecutter.sh
@@ -0,0 +1,13 @@
+if [ $# -eq 0 ]; then
+    printf "Choose a cookiecutter template!\n\n"
+    echo "Options:"
+    for TEMPLATE in "$COOKIECUTTER_DIR"/*; do
+        TEMPLATE=$(basename "$TEMPLATE")
+        echo "  - ${TEMPLATE}"
+    done
+    exit 1
+else
+    CHOSEN_TEMPLATE="$1"
+    echo "$COOKIECUTTER_DIR"
+    cookiecutter "$COOKIECUTTER_DIR/$CHOSEN_TEMPLATE"
+fi
--- flake.nix
+++ flake.nix
@@ -6,19 +6,27 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
-      in {
-        packages.default = pkgs.stdenv.mkDerivation {
-          name = "cookiecutter";
-          src = ./.;
-          buildInputs = [ pkgs.python3Packages.cookiecutter ];
-          buildPhase = ''
-            mkdir -p $out/bin
-            cp -r $src/* $out
-            ln -s ${pkgs.cookiecutter}/bin/cookiecutter $out/bin/cookiecutter
-          '';
-        };
-      });
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in rec {
+      packages.default = pkgs.writeShellApplication {
+        name = "cookiecutter";
+        text = ''
+          COOKIECUTTER_DIR=${./src}
+          ${builtins.readFile ./cookiecutter.sh}
+        '';
+      };
+      apps.default = flake-utils.lib.mkApp {
+        name = "cookiecutter";
+        drv = packages.default;
+      };
+      devShells = pkgs.mkShell {
+        inputsFrom = packages.default;
+      };
+    });
 }
